c62278e7f3cf36dafa2ff759b1a231d9a0dec8aa
jafar rezaei <bomber.man87@yahoo.com>
2019-10-10 13:28:19 +0330

    redux-form

--- packages/redux-form/src/index.js
+++ packages/redux-form/src/index.js
@@ -1,9 +1,42 @@
 import React from "react";
-import HookForm from "./HookForm";
+
+import { createStore, combineReducers } from 'redux'
+import { Provider } from 'react-redux'
+import { reducer as formReducer } from 'redux-form'
+
+
+import ContactForm from './ContactForm'
+
+class ContactPage extends React.Component {
+  submit = values => {
+    // print the form values to the console
+    console.log(values)
+  }
+  render() {
+    return <ContactForm onSubmit={this.submit} />
+  }
+}
+
+
+
+
+const rootReducer = combineReducers({
+  // ...your other reducers here
+  // you have to pass formReducer under 'form' key,
+  // for custom keys look up the docs for 'getFormState'
+  form: formReducer
+})
+
+const store = createStore(rootReducer)
+
 
 // usage of component
 const usage = () => (
-    <HookForm />
+	<Provider store={store}>
+    <ContactPage />
+  </Provider>
 );
 
+
 export default usage;
+
--- packages/redux-form/stories/index.story.js
+++ packages/redux-form/stories/index.story.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 
-import HookForm from './../src';
+import Usage from './../src';
 
 storiesOf('Redux-Form', module)
     .add(
         'default',
-        () => <HookForm />
+        () => <Usage />
     );
